b96a2b20477ab3b68cd9b5f13cb2301764eb7302
shmuelblau <a0528075188@gmail.com>
2025-05-12 15:47:24 +0300

    Set series

--- c#/תרגילים/SeriesAnalyzer/SeriesAnalyzer/Program.cs
+++ c#/תרגילים/SeriesAnalyzer/SeriesAnalyzer/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace SeriesAnalyzer
 {
+
+    class Series 
+    {
+        public int[] series;
+
+       // public bool SetSeries(string[] str) { }
+       // public bool SetSeries(string str) { }
+
+
+
+    }
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            Console.WriteLine(args[2]);
         }
     }
 }
