007fc3e2b459b1816c56dc3383820ff8c0b0e703
shmuelblau <a0528075188@gmail.com>
2025-06-17 14:58:23 +0300

    m

---
+++ c#/תרגילים/Investigation/Investigation/Classes/DTO/SensorDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation.Classes.DTO
+{
+    public class SensorDTO
+    {
+        public int AgentId { get; set; }
+        public string SensorName { get; set; }
+    }
+}
